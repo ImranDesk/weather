ffb469106f0fafcfefc02d57b9cf6dc83800b309
imrandesk <syed16imran@gmail.com>
2022-03-23 12:27:46 +0530

    styling complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [weather, setWeather] = useState(null);
@@ -34,22 +34,38 @@ function App() {
       {weather && (
         <div className="box">
           <div className="search">
-            <input placeholder="Enter City" onChange={weatherInput} type="text" name="" id="" />
+            <input
+              placeholder="Enter City"
+              onChange={weatherInput}
+              type="text"
+              name=""
+              id=""
+            />
             <button onClick={searchWeather}>Search</button>
           </div>
 
           <div className="weather-info">
-            <h4>
-              {weather.location.name}, {weather.location.region},{" "}
-              {weather.location.country}
-            </h4>
+            <div className="card">
+              <div className="cd">
+              <h5>
+                {weather.location.name}, {weather.location.region},{" "}
+              </h5>
 
-            <img src={weather.current.condition.icon} alt="" />
-            <h3>{weather.current.temp_c} ° C</h3>
-            <h3>{weather.current.condition.text}</h3>
-           
+              <h5>{weather.location.country}</h5>
+              </div>
+            </div>
+            <div className="temp">
+              <div className="icon">
+                <img src={weather.current.condition.icon} alt="" />
+              </div>
+              <div className="t-data">
+                <h3>Temp : {weather.current.temp_c}°c</h3>
+                <h5>Feels like : {weather.current.feelslike_c}°c</h5>
+                <p>{weather.current.condition.text}</p>
+                <p>{weather.location.localtime}</p>
+              </div>
+            </div>
 
-            <h4>{weather.location.localtime}</h4>
             <div className="condition"></div>
           </div>
         </div>
